50949c9c6df4e17b62865a4f51c654b2f38a5747
Joshua <joshcscurfield@gmail.com>
2023-09-28 16:58:54 +0100

    initial push
    first push to github

---
+++ app/(dashboard)/todos/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const ErrorUI = () => {
+    return (
+        <div>Error!</div>
+    )
+}
+
+export default ErrorUI
---
+++ app/docs/[[...id]]/page.tsx
@@ -0,0 +1,6 @@
+const DocsPage = ({ params }) => {
+    console.log(params)
+    return <div>hello world</div>
+}
+
+export default DocsPage
